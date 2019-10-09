be0083e52fe7076c936f9e2ded1cc664eda54d43
Ayan Mitra <ayan.mitra@asu.edu>
2019-10-09 11:21:04 -0700

    initial

--- CloudCapTileConverter/Form1.Designer.cs
+++ CloudCapTileConverter/Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "http://gis.azland.gov/arcgis/rest/services/CivilAirPatrol/CivilAirPatrol/MapServe" +
+    "r";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please add in your arcgis server map service";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Pick index xml file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Please pick the cloudcap index.xml file";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 244);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(423, 143);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert CloudCap Cache";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(262, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "W7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Please select a Level to process";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 399);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- CloudCapTileConverter/Form1.cs
+++ CloudCapTileConverter/Form1.cs
@@ -3,18 +3,127 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace CloudCapTileConverter
 {
     public partial class Form1 : Form
     {
+        public string xmlPath = "";
+        public string imgFolder = "";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "Reading index.xml file. Please Wait... " + Environment.NewLine;
+
+            OpenFileDialog renderExcelOFD = new OpenFileDialog();
+
+            renderExcelOFD.InitialDirectory = "C:\\data\\cache_test_l7";
+            renderExcelOFD.Filter = "xml files (*.xml)|*xml|ossim session files (*.session)|*.session|All files (*.*)|*.*";
+            renderExcelOFD.FilterIndex = 1;
+            renderExcelOFD.RestoreDirectory = true;
+            if (renderExcelOFD.ShowDialog() == DialogResult.OK)
+            {
+                string ccXmlPath = renderExcelOFD.FileName;
+                xmlPath = ccXmlPath;
+                imgFolder = Path.GetDirectoryName(renderExcelOFD.FileName);
+               
+                richTextBox1.Text += "Sucessfully Read xml File at " + Environment.NewLine + ccXmlPath + Environment.NewLine;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += "Processing index xml File at " + Environment.NewLine + xmlPath + Environment.NewLine;
+            string regionId = textBox2.Text;
+
+            XElement xIndex = XElement.Load(xmlPath);
+            IEnumerable<FileRequestInfo> cacheReqs = from item in xIndex.Descendants("ImageDescriptor")
+                                          where item.Element("FilePath").ToString().Contains(regionId)
+                                          select new FileRequestInfo { fileHandle= item.Element("FilePath").Value, cdataPayload= item.Element("Extents").Value.Trim() };
+
+            List<FileRequestInfo> cacheRequests =  cacheReqs.ToList<FileRequestInfo>();
+            richTextBox1.Text += "Index file processed. We will now process " + cacheRequests.Count.ToString() + " entries." + Environment.NewLine+ Environment.NewLine;
+
+            foreach(var req in cacheRequests) {
+                runArcgisRequest(req);
+            }
+        }
+
+        private void runArcgisRequest(FileRequestInfo tempFileInfo) {
+            string agUrl = textBox1.Text;
+            //for some reason the data is stored as cdata, so clean the payload & parse xml
+            string cleanXmlStr = Regex.Replace(tempFileInfo.cdataPayload, @"\t|\n|\r", "");
+
+            XElement extentXml = XElement.Parse(cleanXmlStr);
+            ExtentInfo cacheReqsExt = (from item in extentXml.DescendantsAndSelf()
+                            select new ExtentInfo { minLat = (string)item.Element("minLat"),
+                                maxLat = (string)item.Element("maxLat"),
+                                minLon = (string)item.Element("minLon"),
+                                maxLon = (string)item.Element("maxLon")
+                            }).First();
+          
+            string agsPayload = agUrl + "/export?bbox="+ cacheReqsExt.minLon + "%2C"+ cacheReqsExt.minLat + "%2C"+ cacheReqsExt.maxLon + "%2C"+ cacheReqsExt.maxLat + "&bboxSR=4326&layers=&layerDefs=&size=1024%2C1024&imageSR=4326&format=jpg&transparent=false&dpi=&time=&layerTimeOptions=&dynamicLayers=&gdbVersion=&mapScale=&rotation=&f=image";
+            richTextBox1.Text += agsPayload + Environment.NewLine + Environment.NewLine;
+            GetImgAsync(agsPayload, tempFileInfo.fileHandle);
+
+        }
+
+
+        private async Task GetImgAsync(string uri , string fileInfo)
+        {
+            try
+            {
+                using (System.Net.WebClient webClient = new System.Net.WebClient())
+                {
+                    string downloadToDirectory = imgFolder + "\\" + fileInfo;
+                    //set proxy or crdentials here
+                    webClient.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
+                    await webClient.DownloadFileTaskAsync(new Uri(uri), @downloadToDirectory);                   
+                }
+            }
+            catch (Exception)
+            {
+                richTextBox1.Text += "Failed to download File: " + fileInfo;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+        class ExtentInfo
+        {
+            public string minLat { get; set; }
+            public string maxLat { get; set; }
+            public string minLon { get; set; }
+            public string maxLon { get; set; }
+            
+        }
+        class FileRequestInfo {
+            public string fileHandle { get; set; }
+            public string cdataPayload { get; set; }
+        }
     }
+
 }
